10352db4a3b5955348f541a33054644a5be16016
caroscaj <caroscaj@mail.uc.edu>
2022-10-20 12:40:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int row = 1; row <= rows; row++) {
+
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
